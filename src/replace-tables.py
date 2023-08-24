db5b2069ac5ce1e12c07b62b2fa8037a17e37fa6
Kevin Nguyen <nguyenkevin625@gmail.com>
2023-08-23 20:40:59 -0700

    Update Token Permissions

--- src/replace-tables.py
+++ src/replace-tables.py
@@ -9,10 +9,8 @@ with open('README.md', 'r') as readme:
     ps5_compatibility_replace_txt = "--Playstation 5 Compatibility Table--"
     data = data.replace(ps5_compatibility_replace_txt, ps5_compatibility.to_markdown())
 
-
     usb_passthrough_devices_replace_text = "--USB Passthrough Authentication Device Table--"
     data = data.replace(usb_passthrough_devices_replace_text, usb_passthrough_devices.to_markdown())
-
 
 with open(r'README.md', 'w') as readme:
     readme.write(data)
