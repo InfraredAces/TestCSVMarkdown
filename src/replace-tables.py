3a58d6556bd7fdfd133950ace9fc391021cc3b31
Kevin Nguyen <nguyenkevin625@gmail.com>
2023-08-23 21:16:48 -0700

    Remove indexes from tables

--- src/replace-tables.py
+++ src/replace-tables.py
@@ -7,10 +7,9 @@ with open('README.md', 'r') as readme:
     data = readme.read()
 
     ps5_compatibility_replace_txt = "--Playstation 5 Compatibility Table--"
-    data = data.replace(ps5_compatibility_replace_txt, ps5_compatibility.to_markdown())
+    data = data.replace(ps5_compatibility_replace_txt, ps5_compatibility.to_markdown(index=False))
 
     usb_passthrough_devices_replace_text = "--USB Passthrough Authentication Device Table--"
-    data = data.replace(usb_passthrough_devices_replace_text, usb_passthrough_devices.to_markdown())
-
+    data = data.replace(usb_passthrough_devices_replace_text, usb_passthrough_devices.to_markdown(index=False))
 with open(r'README.md', 'w') as readme:
     readme.write(data)
